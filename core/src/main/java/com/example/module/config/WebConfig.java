cec4d1b61c9c8ed0dd1752fcf325b6652619e8b8
giraffeworl <giraffeworld@naver.com>
2024-11-14 17:11:52 +0900

    설정 - aws 탄력적 ip로 변경

--- core/src/main/java/com/example/module/config/WebConfig.java
+++ core/src/main/java/com/example/module/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://3.37.127.89")
+                .allowedOrigins("http://15.164.40.137")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
